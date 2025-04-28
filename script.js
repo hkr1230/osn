b994ba84b7d11d0c8870ba611291b1a21e2ccebb
hkr1230 <dubiang38@gmail.com>
2025-04-29 08:37:44 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById("canv");
+const ctx = canvas.getContext("2d");
+
+ctx.lineWidth = 3;
+
+ctx.beginPath();
+ctx.moveTo(50, 50);
+ctx.lineTo(200, 50);
+ctx.stroke();
+
+ctx.fillRect(50, 50, 100, 100);
+
+
+ctx.beginPath();
+ctx.arc(150, 150, 50, 0, 2 * Math.PI);
+ctx.stroke();
